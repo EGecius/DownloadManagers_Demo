ec9569c1989b02e0464d74496ad32bea1ed373e6
Egidjus Gecius <e.gecius@gmail.com>
2019-10-26 05:38:36 +0100

    move most code to PermissionsDelegate

--- app/src/main/java/com/spartons/androiddownloadmanager/ExternalStorageActivity.kt
+++ app/src/main/java/com/spartons/androiddownloadmanager/ExternalStorageActivity.kt
@@ -1,10 +1,6 @@
 package com.spartons.androiddownloadmanager
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.downloadmanagers_demo.R
 import com.spartons.androiddownloadmanager.DirectoryHelper.Companion.ROOT_DIRECTORY_NAME
@@ -13,17 +9,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class ExternalStorageActivity : AppCompatActivity() {
 
-    lateinit var directoryHelper : DirectoryHelper
-
     lateinit var permissionsDelegate: PermissionsDelegate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        directoryHelper = DirectoryHelper(this)
+        permissionsDelegate = PermissionsDelegate(applicationContext)
 
         setListeners()
-        requestExternalDirectory()
     }
 
     private fun setListeners() {
@@ -59,38 +52,19 @@ class ExternalStorageActivity : AppCompatActivity() {
         startService(getDownloadService(this, SONG_DOWNLOAD_PATH, "$ROOT_DIRECTORY_NAME/"))
     }
 
-    private fun requestExternalDirectory() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            askForPermission()
-        } else {
-            directoryHelper.createDirectoryIfMissing()
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun askForPermission() {
-        requestPermissions(
-            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-            WRITE_EXTERNAL_STORAGE_REQUEST_CODE
-        )
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                directoryHelper.createDirectoryIfMissing()
-        }
+        permissionsDelegate.onRequestPermissionsResult(requestCode, grantResults)
     }
+
 
     companion object {
         private const val PDF_DOWNLOAD_PATH =
             "https://www.goldmansachs.com/insights/pages/gs-research/taking-the-heat/report.pdf"
         private const val SONG_DOWNLOAD_PATH = "https://cloudup.com/files/inYVmLryD4p/download"
-        private const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 54654
     }
 }
--- app/src/main/java/com/spartons/androiddownloadmanager/PermissionsDelegate.kt
+++ app/src/main/java/com/spartons/androiddownloadmanager/PermissionsDelegate.kt
@@ -1,12 +1,53 @@
 package com.spartons.androiddownloadmanager
 
-class PermissionsDelegate {
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+class PermissionsDelegate(private val activity: AppCompatActivity) {
+
+    private val directoryHelper = DirectoryHelper(activity)
 
     fun request(listener: Listener) {
+        requestExternalDirectory()
 
     }
 
+
+    private fun requestExternalDirectory() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            askForPermission()
+        } else {
+            directoryHelper.createDirectoryIfMissing()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun askForPermission() {
+        activity.requestPermissions(
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            WRITE_EXTERNAL_STORAGE_REQUEST_CODE
+        )
+    }
+
+    fun onRequestPermissionsResult(requestCode: Int, grantResults: IntArray) {
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                directoryHelper.createDirectoryIfMissing()
+        }
+    }
+
+
     interface Listener {
         fun onPermissionGranted()
+    }
+
+    companion object {
+
+        private const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 54654
+
+
     }
 }
